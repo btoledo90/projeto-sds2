9ae918b4bb8605334b1564c9a7c434b76deaffda
Bruno de Toledo Queiroz <brunotq19@hotmail.com>
2021-01-09 13:50:47 -0300

    Add routes and StepsHeader

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,6 +1,8 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
+
 
 
 function Home() {
@@ -11,16 +13,16 @@ function Home() {
                     <div className="home-actions">
                         <h1 className="home-title">
                             Faça seu pedido <br />que entregamos <br />pra você!!!
-                </h1>
+                        </h1>
 
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />
-                levaremoss na sua porta
-                </h3>
+                        levaremoss na sua porta
+                        </h3>
 
-                        <a href="orders" className="home-btn-order">
+                        <Link to ="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                </a>
+                        </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
@@ -30,7 +32,7 @@ function Home() {
                 </div>
             </div>
 
-            <Footer/>
+            <Footer />
 
 
         </>
---
+++ src/Orders/StepsHeader.tsx
@@ -0,0 +1,37 @@
+import './styles.css';
+
+function StepsHeader(){
+    return (
+
+        <header className="orders-steps-container">
+            <div className="orders-steps-content">
+                <h1 className="steps-title">
+                    SIGA AS <br /> ETAPAS
+                </h1>
+                <ul className="steps-items">
+                    <li>
+                        <span className="stetps-number">1</span>
+                        Selecione os produtos e localização
+                    </li>
+                    <li>
+                    <span className="stetps-number">2</span>
+                    Depois clique em <strong>"ENVIAR PEDIDO"</strong>
+
+                    </li>
+
+                </ul>
+
+
+            </div>
+    
+
+        </header>
+
+        
+        
+    )
+
+
+}
+
+export default StepsHeader;
