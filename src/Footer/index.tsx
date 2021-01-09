64912b8e3cff66866233bf827ebef33aed78ab47
Bruno de Toledo Queiroz <brunotq19@hotmail.com>
2021-01-09 12:35:07 -0300

    Add Footer

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -10,7 +10,22 @@ function Footer() {
 
 
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
-            
+            <div className="footer-icons"></div>
+
+            <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                <YoutubeIcon />
+
+            </a>
+
+            <a href="https://www.linkedin.com/school/devsuperior" target="_new">
+                <LinkedinIcon />
+
+            </a>
+
+            <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                <InstagramIcon />
+
+            </a>
 
 
        </footer>
